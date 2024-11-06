f0881ce595f07586a34e3a4998de671d9bae3b9d
mardev <contact@starscripts.xyz>
2024-11-06 14:20:59 +0100

    feat(server): added optimalisation + and removed unused variables

--- server/main.lua
+++ server/main.lua
@@ -1,34 +1,30 @@
-RegisterServerEvent('esx_accessories:pay')
-AddEventHandler('esx_accessories:pay', function()
+RegisterNetEvent('esx_accessories:pay', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
 
 	xPlayer.removeMoney(Config.Price, "Accessory Purchase")
 	TriggerClientEvent('esx:showNotification', source, TranslateCap('you_paid', ESX.Math.GroupDigits(Config.Price)))
 end)
 
-RegisterServerEvent('esx_accessories:save')
-AddEventHandler('esx_accessories:save', function(skin, accessory)
+RegisterNetEvent('esx_accessories:save', function(skin, accessory)
 	local source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
+	local lowerAccessory = accessory:lower()
 
-	TriggerEvent('esx_datastore:getDataStore', 'user_' .. string.lower(accessory), xPlayer.identifier, function(store)
+	TriggerEvent('esx_datastore:getDataStore', ('user_%s'):format(lowerAccessory), xPlayer.identifier, function(store)
 		store.set('has' .. accessory, true)
 
-		local itemSkin = {}
-		local item1 = string.lower(accessory) .. '_1'
-		local item2 = string.lower(accessory) .. '_2'
-		itemSkin[item1] = skin[item1]
-		itemSkin[item2] = skin[item2]
-
-		store.set('skin', itemSkin)
+		store.set('skin', {
+			[('%s_1'):format(lowerAccessory)] = skin[('%s_1'):format(lowerAccessory)],
+			[('%s_2'):format(lowerAccessory)] = skin[('%s_2'):format(lowerAccessory)],
+		})
 	end)
 end)
 
 ESX.RegisterServerCallback('esx_accessories:get', function(source, cb, accessory)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	TriggerEvent('esx_datastore:getDataStore', 'user_' .. string.lower(accessory), xPlayer.identifier, function(store)
-		local hasAccessory = (store.get('has' .. accessory) and store.get('has' .. accessory) or false)
+	TriggerEvent('esx_datastore:getDataStore', ('user_%s'):format(accessory:lower()), xPlayer.identifier, function(store)
+		local hasAccessory = (store.get(('has%s'):format(accessory)) and store.get('has' .. ('has%s'):format(accessory)) or false)
 		local skin = (store.get('skin') and store.get('skin') or {})
 
 		cb(hasAccessory, skin)
